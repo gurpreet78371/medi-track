20a8f63f085ac1b8462d7784a2269e5b00c72e0c
Gurpreet Singh <gurpreetsingh.be18cse@pec.edu.in>
2022-03-12 15:03:42 +0530

    "user list changes"

--- pages/owner/index.tsx
+++ pages/owner/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import supplychain from "../../ethereum/supplychain";
 import { useState, useEffect } from "react";
 
-export default function register({usersInfo}) {
+export default function register({ usersInfo }) {
   const [scroll, setScroll] = useState(false);
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -32,6 +32,21 @@ export default function register({usersInfo}) {
       <div className="content">
         <div className="container" style={{ maxWidth: "80%" }}>
           <div className="table-responsive custom-table-responsive">
+            <div className="filter-box">
+              <div className="filter">
+                <label htmlFor="filter" className="filter-label">
+                  Filter:
+                </label>
+                <select name="filter" id="filter">
+                  <option value="all">All Users</option>
+                  <option value="manufacturer">Manufacturer</option>
+                  <option value="wholesaler">Wholesaler</option>
+                  <option value="distributer">Distributer</option>
+                  <option value="pharma">Pharma</option>
+                  <option value="transporter">Transporter</option>
+                </select>
+              </div>
+            </div>
             <table className="table custom-table">
               <thead>
                 <tr>
@@ -43,86 +58,24 @@ export default function register({usersInfo}) {
                 </tr>
               </thead>
               <tbody>
-                {usersInfo.map((userobj,index)=>{
-                    return (
-                        <tr key={userobj.ethAddress}>
-                            <td>{index+1}</td>
-                            <td>
-                                <a href="#">{userobj.name}</a>
-                            </td>
-                            <td>
-                                {userobj.ethAddress}
-                            </td>
-                            <td>{userobj.location}</td>
-                            <td>{userobj.role}</td>
-                        </tr>
-                    );
+                {usersInfo.map((userobj, index) => {
+                  return (
+                    <>
+                      <tr className="spacer">
+                        <td colSpan={100}></td>
+                      </tr>
+                      <tr key={userobj.ethAddress}>
+                        <td>{index + 1}</td>
+                        <td>
+                          <a href="#">{userobj.name}</a>
+                        </td>
+                        <td>{userobj.ethAddress}</td>
+                        <td>{userobj.location}</td>
+                        <td>{userobj.role}</td>
+                      </tr>
+                    </>
+                  );
                 })}
-                <tr>
-                  <td>1</td>
-                  <td>
-                    <a href="#">James Yates</a>
-                  </td>
-                  <td>
-                    Web Designer
-                    <small className="d-block">
-                      Far far away, behind the word mountains
-                    </small>
-                  </td>
-                  <td>+63 983 0962 971</td>
-                  <td>Whole Saler</td>
-                </tr>
-                <tr className="spacer">
-                  <td colSpan={100} />
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>
-                    <a href="#">Matthew Wasil</a>
-                  </td>
-                  <td>
-                    Graphic Designer
-                    <small className="d-block">
-                      Far far away, behind the word mountains
-                    </small>
-                  </td>
-                  <td>+02 020 3994 929</td>
-                  <td>Chemist</td>
-                </tr>
-                <tr className="spacer">
-                  <td colSpan={100} />
-                </tr>
-                <tr>
-                  <td>3</td>
-                  <td>
-                    <a href="#">Sampson Murphy</a>
-                  </td>
-                  <td>
-                    Mobile Dev
-                    <small className="d-block">
-                      Far far away, behind the word mountains
-                    </small>
-                  </td>
-                  <td>+01 352 1125 0192</td>
-                  <td>Whole Saler</td>
-                </tr>
-                <tr className="spacer">
-                  <td colSpan={100} />
-                </tr>
-                <tr>
-                  <td>4</td>
-                  <td>
-                    <a href="#">Gaspar Semenov</a>
-                  </td>
-                  <td>
-                    Illustrator
-                    <small className="d-block">
-                      Far far away, behind the word mountains
-                    </small>
-                  </td>
-                  <td>+92 020 3994 929</td>
-                  <td>Pharmaceutical Company</td>
-                </tr>
               </tbody>
             </table>
           </div>
@@ -132,38 +85,36 @@ export default function register({usersInfo}) {
   );
 }
 
-export async function getStaticProps(){
-    const users=await supplychain.methods.getUsers().call();
-    let usersInfo=[];
-    for (let user in users){
-        const userInfo=await supplychain.methods.getUserInfo(users[parseInt(user)]).call();
-        let rolestr;
-        if(userInfo.role==1){
-            rolestr="Manufacturer";
-        }
-        else if(userInfo.role==2){
-            rolestr="Wholesaler";
-        }
-        else if(userInfo.role==3){
-            rolestr="Distributer";
-        }
-        else if(userInfo.role==4){
-            rolestr="Pharma";
-        }
-        else if(userInfo.role==5){
-            rolestr="Transporter";
-        }
-        let userobj={
-            name: userInfo.name,
-            location: userInfo.location,
-            ethAddress:userInfo.ethAddress,
-            role: rolestr
-        };
-        usersInfo.push(userobj);
+export async function getStaticProps() {
+  const users = await supplychain.methods.getUsers().call();
+  let usersInfo = [];
+  for (let user in users) {
+    const userInfo = await supplychain.methods
+      .getUserInfo(users[parseInt(user)])
+      .call();
+    let rolestr;
+    if (userInfo.role == 1) {
+      rolestr = "Manufacturer";
+    } else if (userInfo.role == 2) {
+      rolestr = "Wholesaler";
+    } else if (userInfo.role == 3) {
+      rolestr = "Distributer";
+    } else if (userInfo.role == 4) {
+      rolestr = "Pharma";
+    } else if (userInfo.role == 5) {
+      rolestr = "Transporter";
     }
-    return {
-        props:{
-            usersInfo
-        }
-    }
+    let userobj = {
+      name: userInfo.name,
+      location: userInfo.location,
+      ethAddress: userInfo.ethAddress,
+      role: rolestr,
+    };
+    usersInfo.push(userobj);
+  }
+  return {
+    props: {
+      usersInfo,
+    },
+  };
 }
