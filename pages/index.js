351350a6bd4b4a46a5ef67744717619d67ab5f6f
Gurpreet Singh <gurpreetsingh.be18cse@pec.edu.in>
2022-03-12 16:38:32 +0530

    "solved bootstrap issue"

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Head from "next/head";
-import web3 from '../ethereum/web3'
-import supplychain from '../ethereum/supplychain';
+import web3 from "../ethereum/web3";
+import supplychain from "../ethereum/supplychain";
 import { useState, useEffect } from "react";
-import "bootstrap/dist/js/bootstrap.js";
 
 export default function index() {
-  const [address,setAddress]=useState('');
-  const [role,setRole]=useState('');
+  const [address, setAddress] = useState("");
+  const [role, setRole] = useState("");
   const [scroll, setScroll] = useState(false);
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -15,61 +14,52 @@ export default function index() {
     });
   }, []);
 
-  const getAddress=async () =>{
-    if(window.ethereum=='undefined'){
-      alert('MetaMask is not installed!!!');
-    }
-    else{
-      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+  const getAddress = async () => {
+    if (window.ethereum == "undefined") {
+      alert("MetaMask is not installed!!!");
+    } else {
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
       const account = web3.utils.toChecksumAddress(accounts[0]);
       setAddress(account);
-      const owner=await supplychain.methods.owner().call();
-      console.log(typeof(account));
-      console.log(typeof(owner));
-      console.log(owner==account);
+      const owner = await supplychain.methods.owner().call();
+      console.log(typeof account);
+      console.log(typeof owner);
+      console.log(owner == account);
       console.log(owner);
       console.log(account);
       console.log();
-      if(owner==account){
-        console.log('You are owner');
-        setRole('owner');
-      }
-      else{
-        const info=await supplychain.methods.getUserInfo(account).call();
+      if (owner == account) {
+        console.log("You are owner");
+        setRole("owner");
+      } else {
+        const info = await supplychain.methods.getUserInfo(account).call();
         console.log(info);
-        if(info.role==1){
-          setRole('manufacturer');
-        }
-        else if(info.role==2){
-          setRole('wholesaler');
-        }
-        else if(info.role==3){
-          setRole('distributer');
-        }
-        else if(info.role==4){
-          setRole('pharma');
-        }
-        else if(info.role==5){
-          setRole('transporter');
+        if (info.role == 1) {
+          setRole("manufacturer");
+        } else if (info.role == 2) {
+          setRole("wholesaler");
+        } else if (info.role == 3) {
+          setRole("distributer");
+        } else if (info.role == 4) {
+          setRole("pharma");
+        } else if (info.role == 5) {
+          setRole("transporter");
         }
       }
     }
-  }
+  };
 
   return (
     <div>
       <Head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-        <script src="/script.js"></script>
         <title>Document</title>
       </Head>
       {/* <!-- navbar section start --> */}
-      <nav className={scroll ? "navbar sticky" : "navbar"}>
+      <nav className={scroll ? "home-navbar sticky" : "home-navbar"}>
         <div className="max-width">
           <div className="logo">
             <a href="#">MediTrack Blockchain Transparent Supply</a>
@@ -207,7 +197,7 @@ export default function index() {
             <img src="/banner.jpeg" alt="" />
           </div>
           <div className="right">
-            <div className="card">
+            <div className="info-card">
               <div className="heading">
                 Establish supply chain integrity with cold chain monitoring
               </div>
@@ -219,7 +209,7 @@ export default function index() {
                 chargebacks where they belong.
               </p>
             </div>
-            <div className="card">
+            <div className="info-card">
               <div className="heading">
                 Establish supply chain integrity with cold chain monitoring
               </div>
@@ -231,7 +221,7 @@ export default function index() {
                 chargebacks where they belong.
               </p>
             </div>
-            <div className="card">
+            <div className="info-card">
               <div className="heading">
                 Establish supply chain integrity with cold chain monitoring
               </div>
@@ -243,7 +233,7 @@ export default function index() {
                 chargebacks where they belong.
               </p>
             </div>
-            <div className="card">
+            <div className="info-card">
               <div className="heading">
                 Establish supply chain integrity with cold chain monitoring
               </div>
