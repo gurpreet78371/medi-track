4d5b8bf8de9690c3bbb9752d03e3fc6d351f3990
Gurpreet Singh <gurpreetsingh.be18cse@pec.edu.in>
2022-03-13 21:49:23 +0530

    inserted map

--- pages/medicine_info.js
+++ pages/medicine_info.js
@@ -17,8 +17,8 @@ export default function medicine_info() {
           <div className="left-half">
             <h3 className="name">Medicine Name</h3>
             <div className="row1">
-              <div>Name: </div>
-              <div>Medicine</div>
+              <div className="left">Name: </div>
+              <div className="right">Medicine</div>
             </div>
             <div className="row1">
               <div className="left">Quantity: </div>
@@ -46,91 +46,51 @@ export default function medicine_info() {
             </div>
           </div>
           <div className="right-half">
-            <img src="/banner.jpeg" alt="QR code" />
+            <img src="/qr.jpg" alt="QR code" />
           </div>
         </div>
+
         <div className="divider"></div>
+
         <div className="history">
-          <div className="delhivery">
-            <div className="delhivery-card">
-              <div className="title">Purchase Reciept</div>
-              <div className="info">
-                <div className="row">
-                  <div className="col-7">
-                    {" "}
-                    <span id="heading">Date</span>
-                    <br /> <span id="details">10 October 2018</span>{" "}
+          <div className="left">
+            <section className="experience" id="experience">
+              <div className="max-width1">
+                <div className="experience-content">
+                  <div className="outer">
+                    <div className="card">
+                      <div className="experience-details">
+                        <div className="experience-level">
+                          Intern - Network and System Engineer
+                        </div>
+                        <div className="date italic">Jan 2021 - Jun 2021</div>
+                        <div className="company">Cvent</div>
+                      </div>
+                      <div className="square" />
+                    </div>
+                    <div className="circle">
+                      <i className="fas fa-envelope" />
+                    </div>
                   </div>
-                  <div className="col-5 pull-right">
-                    {" "}
-                    <span id="heading">Order No.</span>
-                    <br /> <span id="details">012j1gvs356c</span>{" "}
-                  </div>
-                </div>
-              </div>
-              <div className="pricing">
-                <div className="row">
-                  <div className="col-9">
-                    {" "}
-                    <span id="name">BEATS Solo 3 Wireless Headphones</span>{" "}
-                  </div>
-                  <div className="col-3">
-                    {" "}
-                    <span id="price">£299.99</span>{" "}
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-9">
-                    {" "}
-                    <span id="name">Shipping</span>{" "}
-                  </div>
-                  <div className="col-3">
-                    {" "}
-                    <span id="price">£33.00</span>{" "}
+                  <div className="outer">
+                    <div style={{ marginBottom: "0px" }} className="card">
+                      <div className="experience-details">
+                        <div className="experience-level">Teacher</div>
+                        <div className="date italic">2016 - 2020</div>
+                        <div className="company">Part Time</div>
+                      </div>
+                      <div className="square" />
+                    </div>
+                    <div className="circle">
+                      <i className="fas fa-envelope" />
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className="total">
-                <div className="row">
-                  <div className="col-9" />
-                  <div className="col-3">
-                    <big>£262.99</big>
-                  </div>
-                </div>
-              </div>
-              <div className="tracking">
-                <div className="title">Tracking Order</div>
-              </div>
-              <div className="progress-track">
-                <ul id="progressbar">
-                  <li className="step0 active " id="step1">
-                    Ordered
-                  </li>
-                  <li className="step0 active text-center" id="step2">
-                    Shipped
-                  </li>
-                  <li className="step0 active text-right" id="step3">
-                    On the way
-                  </li>
-                  <li className="step0 text-right" id="step4">
-                    Delivered
-                  </li>
-                </ul>
-              </div>
-              <div className="footer">
-                <div className="row">
-                  <div className="col-2">
-                    <img
-                      className="img-fluid"
-                      src="https://i.imgur.com/YBWc55P.png"
-                    />
-                  </div>
-                  <div className="col-10">
-                    Want any help? Please &nbsp;<a> contact us</a>
-                  </div>
-                </div>
-              </div>
-            </div>
+            </section>
+          </div>
+          <div className="right">
+            <img src="/map.gif" alt="location" />
           </div>
         </div>
       </div>
