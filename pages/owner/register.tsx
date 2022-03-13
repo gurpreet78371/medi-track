09a910b3150c3d6202c7ba5239cff8f10af63775
Gurpreet Singh <gurpreetsingh.be18cse@pec.edu.in>
2022-03-13 08:59:10 +0530

    "small changes"

--- pages/owner/register.tsx
+++ pages/owner/register.tsx
@@ -1,49 +1,55 @@
 import React from "react";
 import supplychain from "../../ethereum/supplychain";
 import NavBar from "../../components/NavBar";
-import {useState,useEffect} from "react";
+import { useState, useEffect } from "react";
 import web3 from "../../ethereum/web3";
 
 export default function register() {
-  const [address,setAddress]=useState('');
-    const [formvalues,setformvalues]=useState({
-        name: '',
-        address: '',
-        role: '',
-        location: ''
-    });
-    const links = [
-      { name: "Register", address: "#", active: true },
-      { name: "User", address: "/owner", active: false },
-    ];
-    useEffect(async ()=>{
-        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-        const account = web3.utils.toChecksumAddress(accounts[0]);
-        const owner=await supplychain.methods.owner().call();
-        if(account!=owner){
-            alert("You are not a owner");
-        }
-        else{
-            // alert("You are owner");
-            setAddress(account);
-        }
-    },[])
-    const registerUser=async (event)=>{
-        event.preventDefault();
-        console.log(formvalues);
-        await supplychain.methods.registerUser(formvalues.address,formvalues.name,formvalues.location,formvalues.role).send({
-            from: address
-        });
+  const [address, setAddress] = useState("");
+  const [formvalues, setformvalues] = useState({
+    name: "",
+    address: "",
+    role: "",
+    location: "",
+  });
+  const links = [
+    { name: "Register", address: "#", active: true },
+    { name: "User", address: "/owner", active: false },
+  ];
+  useEffect(async () => {
+    const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+    const account = web3.utils.toChecksumAddress(accounts[0]);
+    const owner = await supplychain.methods.owner().call();
+    if (account != owner) {
+      alert("You are not a owner");
+    } else {
+      // alert("You are owner");
+      setAddress(account);
     }
-    const getUsers=async()=>{
-        const users=await supplychain.methods.getUsers().call();
-        console.log(users);
-        getUserInfo(users[0]);
-    }
-    const getUserInfo=async(address)=>{
-        const user=await supplychain.methods.getUserInfo(address).call();
-        console.log(user);
-    }
+  }, []);
+  const registerUser = async (event) => {
+    event.preventDefault();
+    console.log(formvalues);
+    await supplychain.methods
+      .registerUser(
+        formvalues.address,
+        formvalues.name,
+        formvalues.location,
+        formvalues.role
+      )
+      .send({
+        from: address,
+      });
+  };
+  const getUsers = async () => {
+    const users = await supplychain.methods.getUsers().call();
+    console.log(users);
+    getUserInfo(users[0]);
+  };
+  const getUserInfo = async (address) => {
+    const user = await supplychain.methods.getUserInfo(address).call();
+    console.log(user);
+  };
   return (
     <div className="register">
       <NavBar links={links}></NavBar>
@@ -56,7 +62,11 @@ export default function register() {
           </label>
           <input
             type="text"
-            name='name' value={formvalues.name} onChange={(event) =>{setformvalues({...formvalues,name:event.target.value})}}
+            name="name"
+            value={formvalues.name}
+            onChange={(event) => {
+              setformvalues({ ...formvalues, name: event.target.value });
+            }}
             placeholder="Enter User's Name"
             required
           />
@@ -66,7 +76,11 @@ export default function register() {
           <input
             type="text"
             placeholder="Enter User's Ethereum address"
-            name='address' value={formvalues.address} onChange={(event) =>{setformvalues({...formvalues,address:event.target.value})}}
+            name="address"
+            value={formvalues.address}
+            onChange={(event) => {
+              setformvalues({ ...formvalues, address: event.target.value });
+            }}
             minLength={42}
             maxLength={42}
             required
@@ -77,7 +91,11 @@ export default function register() {
           <input
             type="text"
             placeholder="Enter User's Location"
-            name='location' value={formvalues.location} onChange={(event) =>{setformvalues({...formvalues,location:event.target.value})}}
+            name="location"
+            value={formvalues.location}
+            onChange={(event) => {
+              setformvalues({ ...formvalues, location: event.target.value });
+            }}
             data-toggle="modal"
             data-target="#exampleModalLong"
             required
@@ -87,13 +105,21 @@ export default function register() {
           </label>
           <br />
           <div className="btn-group dropright">
-          <select name="role" className="dropdown-menu" value={formvalues.role} onChange={(event) =>{setformvalues({...formvalues,role:event.target.value})}}>
-            <option value={1}>Manufacturer</option>
-            <option value={2}>Wholesaler</option>
-            <option value={3}>Distributer</option>
-            <option value={4}>Pharma</option>
-            <option value={5}>Transporter</option>
-          </select>
+            <select
+              name="role"
+              className="dropdown-menu"
+              style={{ position: "relative" }}
+              value={formvalues.role}
+              onChange={(event) => {
+                setformvalues({ ...formvalues, role: event.target.value });
+              }}
+            >
+              <option value={1}>Manufacturer</option>
+              <option value={2}>Wholesaler</option>
+              <option value={3}>Distributer</option>
+              <option value={4}>Pharma</option>
+              <option value={5}>Transporter</option>
+            </select>
           </div>
           <div className="clearfix">
             <button type="submit" className="btn">
